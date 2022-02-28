6524869a07148672af7511bad1420662091f858f
svetvl <95696137+svetvl@users.noreply.github.com>
2022-02-28 13:24:04 +0500

    Add files via upload

---
+++ SF/Practicum18/config.py
@@ -0,0 +1,16 @@
+TOKEN = '5277021789:AAHeLGt0SIyS0z4JuHHPcTicfhXZSu2hZBw'
+CURRENCY_API = 'a779b3b6ba231da78e1875f3ea8d3d3e6175b5d37dcb1899c5bc56a295ace64d'
+
+
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB',
+    'BTC': 'BTC',
+    'ETH': 'ETH',
+    'USD': 'USD',
+    'EUR': 'EUR',
+    'RUB': 'RUB'
+}
